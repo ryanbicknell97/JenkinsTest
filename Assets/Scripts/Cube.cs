27ce4ab0771274f19b930b31c7e75a8f2a8c352c
Bicknell <ryan.bicknell1@DETNSW>
2016-11-09 11:36:34 +1100

    Deliberately made errors for Jenkins

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        object m = null;
+        string s = m.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
